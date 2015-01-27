81ac5061da2f6305a642d9ff6b3b3b7d2b8ef038
Eric Phung <esphung@iupui.edu>
2015-01-26 23:03:17 -0500

    game is done.  can't compile with gcc on pegasus bcuz c++ does not come with regex standard, therefore player input junction must be built using stdio, instead... wtf...

--- ericPhungGuess.cpp
+++ ericPhungGuess.cpp
@@ -7,9 +7,8 @@
 
 using namespace std;
 
-/* c is used for previous answer comparison */
+
 int a, b, tries = 1;// Declaration of main variables used
-int previousGuesses[] = {};// empty list to store used values
 char arg[] = {};
 int highLimit = 100;
 int lowLimit = 1;
@@ -20,46 +19,40 @@ void tooHigh();
 
 void tooLow(){
 	tries = (tries+1);
-	printf("Tries: %i\n", tries);
-	int lowLimit = b;
-	//printf("new lowLimit: %i\n", lowLimit);
+	lowLimit = b;
 	b = lowLimit + (std::rand() %(highLimit - lowLimit +1));
 	do{
 		/* first guess partition */
 		b = (lowLimit+highLimit)/2;
 	} while (tries < 2);
-	//printf("New guess if b was too low = %i\n", b);
-	printf("\nGuess(%i) = %i\n", tries, b);
 }
 
 
 void tooHigh(){
 	tries = (tries+1);
-	printf("Tries: %i\n", tries);
 	highLimit = b;
-	//printf("new highLimit %i\n", highLimit);	
 	b = lowLimit + (std::rand() %(highLimit - lowLimit));
 	do{
 		/* first guess partition */
 		b = (lowLimit+highLimit)/2;
 	} while (tries < 2);
-	printf("\nGuess(%i) = %i\n", tries, b);
+	//printf("\nGuess(%i) = %i\n", tries, b);
 }
 
 
 void guessCompare(){
 	if (a == b)
 	{
-		/* code */
+		/* evaluation of user input against Ai */
 		cout << "==== I win! ====" << endl;
 		exit(1);
 	}
-	cout << "Is my previous guess too high or too low?\n" << endl;
+	cout << "Is this guess too high or too low?\n(type 'High' or 'Low')" << endl;
 	cin >> arg;
 	/* Match argument to 'too high' or 'too low' */
-	regex highPattern("(.*)high?.*|.*high?.*", regex_constants::icase);//Match "high"
-	bool highMatched = regex_match(arg, highPattern);
-	cout << (highMatched? "High Matched" : "High Not Matched") << endl;
+	std::regex highPattern("(.*)high?.*|.*high?.*", regex_constants::icase);
+	bool highMatched = std::regex_match(arg, highPattern);
+	//cout << (highMatched? "High Matched" : "High Not Matched") << endl;
 	
 	if (highMatched == TRUE){
 		//printf("Too high = True\n");
@@ -71,7 +64,8 @@ void guessCompare(){
 		/* possiblle if guess was not too high */
 		regex lowPattern("(.*)low?.*|.*low?.*", regex_constants::icase);//Match "low"
 		bool lowMatched = regex_match(arg, lowPattern);
-		cout << (lowMatched? "Low Matched" : "Low Not Matched") << endl;
+		//cout << (lowMatched? "Low Matched" : "Low Not Matched") << endl;
+
 		if (lowMatched == TRUE)
 		{
 			/* if guess was too low */
@@ -94,21 +88,24 @@ void guessCompare(){
 }
 
 
-int main(int argc, char const *argv[])
+int main()
 {
 	srand(time(NULL));
 	/* initialize random values */
 	a = rand() % highLimit + lowLimit;// random coorect number for development
 	b = 50;// initialize first guess as two possible partitions
 
-	printf("Correct = %i\n", a);// SHow correct number for dev
-	printf("\nGuess(%i) = %i\n", tries, b);// show initial stats
+	
+	
 
 	/* main loop passes thru 7 times but says eight to make up for humans */
-	while(tries <= 8) {
+	while(tries <= MAX) {
 	    /* code */
+	    //printf("Correct = %i\n", a);// SHow correct number for dev
+	    cout << "Correct: 	" << a << endl;
+	    //printf("\nGuess(%i) = %i\n", tries, b);// show initial stats
+	    cout << "Guess(" << tries << ")	" << b << endl;
 	    guessCompare();
-	    printf("correct: %i\nhighLimit: %i\nlowLimit: %i\n", a, highLimit, lowLimit);// show cpu guessing range for dev
 
 	}
 	
